8c9015ddb713e27100e50b10c0f7852956ad83d4
AbdulGoniyy <kelani.abdganiyy1@gmail.com>
2024-06-12 12:30:43 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function toggleDropdownMenu() {
   var dropdownMenu = document.getElementById('dropdownMenu');
   if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
@@ -7,3 +6,8 @@ function toggleDropdownMenu() {
     dropdownMenu.style.display = 'none';
   }
 }
+
+// Ensure the dropdown menu is hidden on page load
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('dropdownMenu').style.display = 'none';
+});
